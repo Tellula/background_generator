a9a8457df72ec7554c2275542c0df613f07e143d
Tellula <52374386+Tellula@users.noreply.github.com>
2022-06-09 17:55:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
+var _ = require('lodash');
+
+
+
+const array = [1,2,3,4,5,6,7,8,9];
+console.log('answer :', _.without(array, 3))
+
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 const random = document.getElementById("random");
 
-
-
 function setGradient() {
-    body.style.background = 
-    "linear-gradient(to right, "
-    + color1.value 
-    + "," 
-    + color2.value 
-    + ")";
-    css.textContent = body.style.background + ";";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+  css.textContent = "background: " + body.style.background + ";";
 }
 
-
-function setRandomBG(){
-    const randomColor1 = Math.floor(Math.random()*16777215).toString(16);
-    const randomColor2 = Math.floor(Math.random()*16777215).toString(16);
-    let randColor1 = "#" + randomColor1.padStart(6, 0);
-    let randColor2 = "#" + randomColor2.padStart(6, 0);
-    console.log(randColor1,randColor2);
-    color1.value = randColor1;
-    color2.value = randColor2;
-    setGradient();
+function setRandomBG() {
+  const randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+  let randColor1 = "#" + randomColor1.padStart(6, 0);
+  let randColor2 = "#" + randomColor2.padStart(6, 0);
+  console.log(randColor1, randColor2);
+  color1.value = randColor1;
+  color2.value = randColor2;
+  setGradient();
 }
-
-
-
-
 
 setGradient();
 
 random.addEventListener("click", setRandomBG);
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
-
-
-
-
-
